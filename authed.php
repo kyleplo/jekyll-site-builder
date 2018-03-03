521c712cbea45a94477cb10d5eb4b0d7fb480bc8
kyleplo <31634240+kyleplo@users.noreply.github.com>
2018-03-03 08:43:41 -0500

    Update authed.php

--- authed.php
+++ authed.php
@@ -1,19 +1,4 @@
 <p>Authenticating...</p>
-<script>
-window.addEventListener("load",function (){
-var here = new URL(location.href);
-var state = here.searchParams.get("state");
-var code = here.searchParams.get("code");
-var head = new Headers();
-head.append("Accept","application/json");
-if(state === localStorage.authState){
-fetch("https://github.com/login/oauth/access_token?client_id=<client_id>&client_secret=<client_secret>&code=" + code + "&state=" + localStorage.authState,{method: "POST",headers: head}).then(function (r){return r.json()}).then(function (j){
-localStorage.authState = "ha ha tricked ya the auth state was da le ted";
-localStorage.authToken = j["access_token"];
-location.href = "https://kyleplo.com/jekyll-site-builder/edit.html"
-})
-}})
-</script>
 <?php
 $opts = array(
   'http'=>array(
@@ -28,3 +13,9 @@ $xml=simplexml_load_string($auth) or die("Failed to parse XML from Github server
 echo "<span id=code hidden>" . $xml->access_token . "</span>"
 };
 ?>
+<script>
+window.addEventListener("load",function (){
+localStorage.authCode = document.getElementById("code").innerHTML;
+location.href = "https://kyleplo.com/jekyll-site-builder/edit.html"
+})
+</script>
